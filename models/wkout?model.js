d5a550bcb938f7dc7a91f1c05967e97abe1e383b
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2021-01-06 13:46:28 -0800

    Commiting homework

---
+++ models/wkout?model.js
@@ -0,0 +1,19 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const workoutSchema = new mongoose.Schema(
+//     {
+//         day: {
+//             type: Date,
+//             default: ()=> new Date()
+//         },
+//         exercises: [
+//             {
+//                 type: {
+//                     type: String,
+
+//                 }
+//             }
+//         ]
+//         }
+// )
