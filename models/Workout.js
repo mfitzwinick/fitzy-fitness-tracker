2c97f93ad22d3c142d3d23eccd77501ab4de14da
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2021-01-11 15:29:50 -0800

    fixed Workout Model code

--- models/Workout.js
+++ models/Workout.js
@@ -1,21 +1,39 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
+const WorkoutSchema = new Schema(
     {
         day: {
             type: Date,
             default: ()=> new Date()
-        },
+            },
         exercises: [
             {
                 type: {
                     type: String,
+                    trim: true,
+                    required: "Must specify type"
+                },
+                    name: {
+                        type: String,
+                        trim: true,
+                    required: "Must specify name"
+                    },
+                    duration: {
+                    type:number
+                },
+                    weight: {
+                        type: number
+                    },
+                    reps: {
+                        type: number
+                    },
+                    sets: {
+                        type:number
+                    }
 
-                }
-            }
-        ]
-        }
-)
-const Workouts=mongoose.model("Workout", WorkoutSchema);
-module.exports =Workouts;
+                }]});
+
+                
+const Workout=mongoose.model("Workout", WorkoutSchema);
+module.exports =Workout;
--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,36 @@
 const router = require("express").Router();
-const Workout = require("../public/workout.js");
+const Workout = require("../models");
 
 router.post("/api/workouts",(req,res) => {
-Workout.create({})
-.then(dbWorkout =>{
-    res.json(dbWorkout);
+Workout.create({exercises})
+.then(Workout) => {
+  console.log(WTH?)
+    res.json(WokoutSchema);
 })
 .catch(err => {
     res.json(err)
 });
 });
 
-router.put("/api/workouts/:id",({body,params}, res) =>
+router.put("/api/workouts/:id",({body,params}, res) =>{
+    db.exercises.update(
+      {
+        _id: mongojs.ObjectId(params.id)
+      },
+      {
+        $set: {
+          read: true
+        }
+      },
+  
+      (error, edited) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+        } else {
+          console.log(edited);
+          res.send(edited);
+        }
+      }
+    );
+  });
